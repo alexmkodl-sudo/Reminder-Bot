1d5624787f67ca443ef000eced4b74bbfa5eb20a
Alexandr M. <theraws19@gmail.com>
2026-05-23 08:35:50 +0300

    Delete db directory

--- db/__init__.py
+++
@@ -1,5 +0,0 @@
-from .models import (
-    init_db, ensure_user, get_timezone, set_timezone,
-    add_event, get_upcoming_events, get_all_upcoming_events,
-    delete_event, reminder_already_sent, mark_reminder_sent,
-)
